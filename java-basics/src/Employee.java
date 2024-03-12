2f9c9bdafa1128228a8babc32bfff1dca7cffa74
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-03-12 22:57:03 +0530

    String data type

--- java-basics/src/Employee.java
+++ java-basics/src/Employee.java
@@ -2,5 +2,23 @@ public class Employee {
     public static void main(String[] args) {
         int a = -10;
         System.out.println("abc: " + a);
+        byte c = 123;
+        byte b = 2;
+
+
+        //String literals and comparisons
+        String s1 = "abc";
+        String s2 = "abc";
+        String s3 = new String("abc");
+
+        System.out.println(s1==s2);//true because both points to same heap memory
+        System.out.println(s1.equals(s2));
+
+        System.out.println(s1==s3); //comes out false
+        //because these two s1 and s3 both are at different heap memory locations
+
+
+        System.out.println(s1.equals(s3));//true because value are same
+
     }
 }
