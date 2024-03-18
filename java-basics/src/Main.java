a17764ca51f5db395884414be67d308305090d6b
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-03-18 20:42:29 +0530

    bitwise and logical operators

--- java-basics/src/Main.java
+++ java-basics/src/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Engineer.getProfession();
         System.out.println("tHE eND");
+        System.out.println(5&2); // will come out zero
+//        System.out.println(5 && 2); // will come out 5
     }
 }
