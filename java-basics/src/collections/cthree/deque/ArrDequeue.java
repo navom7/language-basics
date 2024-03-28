4ec3c11e94934e594ea18b2ce0fc670fd6f916ff
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-03-28 23:52:28 +0530

    PriorityBlockingQueue

--- java-basics/src/collections/cthree/deque/ArrDequeue.java
+++ java-basics/src/collections/cthree/deque/ArrDequeue.java
@@ -1,6 +1,7 @@
 package collections.cthree.deque;
 
 import java.util.ArrayDeque;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class ArrDequeue {
     public static void main(String[] args) {
@@ -13,6 +14,15 @@ public class ArrDequeue {
         System.out.println(arrDq.size());
 
         System.out.println(arrDq.poll());
+
+
+        PriorityBlockingQueue<Integer> dq = new PriorityBlockingQueue<>();
+
+        dq.add(1);
+        dq.add(5);
+        dq.add(10);
+        dq.add(10);
+        System.out.println(dq.size());
 
     }
 }
