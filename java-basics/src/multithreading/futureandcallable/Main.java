42b06957b4198933f24e8aeb5da5ca9b64a2b9e0
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-04-11 10:02:15 +0530

    java 8 completablefuture

---
+++ java-basics/src/multithreading/futureandcallable/Main.java
@@ -0,0 +1,40 @@
+package multithreading.futureandcallable;
+
+import java.util.concurrent.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1,1,1, TimeUnit.HOURS, new ArrayBlockingQueue<>(10),
+                Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+
+        Future<?> futureObj = executor.submit(() -> {
+           try{
+               Thread.sleep(7000);
+               System.out.println("This is the task, which thread will execute");
+           } catch (Exception e) {
+
+           }
+        });
+
+        System.out.println("isDone: " + futureObj.isDone());
+
+        try{
+            futureObj.get(2, TimeUnit.SECONDS);
+        }catch (TimeoutException e) {
+            System.out.println("Timeout exception: " + e.getMessage());
+        } catch (Exception e) {
+
+        }
+
+        try{
+            futureObj.get();
+        } catch (Exception e) {
+
+        }
+
+        System.out.println("isDone: " + futureObj.isDone());
+        System.out.println("is cancelled: " + futureObj.isCancelled());
+
+
+    }
+}
--- java-basics/src/multithreading/threadpool/Main.java
+++ java-basics/src/multithreading/threadpool/Main.java
@@ -1,7 +1,47 @@
 package multithreading.threadpool;
 
+import java.util.concurrent.*;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("ThreadPool");
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(2, 4,
+                10, TimeUnit.MINUTES, new ArrayBlockingQueue<>(2),
+                new CustomThreadFactory(),
+                new CustomRejectHandler()
+                );
+
+        executor.allowCoreThreadTimeOut(true); //to make sure that keepAliveTIme passed is being used to delete and thread if not used after 10 mins
+
+
+
+        for (int i = 0; i < 7; i++) {
+            executor.submit(() -> {
+                try {
+                    Thread.sleep(5000);
+                } catch (Exception e) {
+                }
+                System.out.println("Task processed by: " + Thread.currentThread().getName());
+            });
+        }
+    }
+
+
+}
+class CustomThreadFactory implements ThreadFactory{
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(r);
+        t.setPriority(Thread.NORM_PRIORITY);
+        t.setDaemon(false);
+        return t;
+    }
+}
+
+class CustomRejectHandler implements RejectedExecutionHandler{
+
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        System.out.println("Task rejected: " + r.toString() + " " + executor.toString());
     }
 }
