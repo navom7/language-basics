c49765a740b38aa21c789244ff20bf1549ea3ab2
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-04-12 10:18:48 +0530

    shutdown

--- java-basics/src/multithreading/shutdown/ShutdownExample.java
+++ java-basics/src/multithreading/shutdown/ShutdownExample.java
@@ -1,4 +1,4 @@
-package multithreading.shutdownAndScheduledThreadPoolExecutor;
+package multithreading.shutdown;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
---
+++ java-basics/src/multithreading/shutdown/ShutdownNowExample.java
@@ -0,0 +1,29 @@
+package multithreading.shutdown;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ShutdownNowExample {
+    public static void main(String[] vargs) {
+        ExecutorService poolExecutor = Executors.newFixedThreadPool(5);
+
+        poolExecutor.submit(() -> {
+            System.out.println("Thread going to start its work");
+            try{
+                Thread.sleep(5000);
+            } catch (Exception e) {
+
+            }
+            System.out.println("Thread completed its work");
+        });
+
+        poolExecutor.shutdownNow();
+
+        System.out.println("already submitted task Thread will execute event after shutdown");
+        poolExecutor.submit(() -> {
+            System.out.println("executor caled after shutdown");
+        });
+
+
+    }
+}
