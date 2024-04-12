5ff2583e008537e69f4c193a40d218036b5381d4
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-04-12 10:31:19 +0530

    schedule with fixed rate and delay

--- java-basics/src/multithreading/schedule/ScheduledTaskExec.java
+++ java-basics/src/multithreading/schedule/ScheduledTaskExec.java
@@ -1,9 +1,6 @@
 package multithreading.schedule;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class ScheduledTaskExec {
     public static void main(String[] args) {
@@ -15,8 +12,34 @@ public class ScheduledTaskExec {
 
 
 
-        poolExecutor.scheduleAtFixedRate(() -> {
+//        this will keep on running so we will have to explicitly stop it
+        Future<?> futureObj = poolExecutor.scheduleAtFixedRate(() -> {
             System.out.println("first delay is 4 sec and after that every 2 sec:");
+            //If this task takes longer than period, then next task will start immediately after this repeatation ends
         }, 4, 2, TimeUnit.SECONDS);
+
+        try {
+            Thread.sleep(10000);
+            futureObj.cancel(true);
+        } catch (Exception e) {
+
+        }
+
+
+
+
+
+//        this will keep on running so we will have to explicitly stop it
+        futureObj = poolExecutor.scheduleWithFixedDelay(() -> {
+            System.out.println("first delay is 4 sec and after that every 2 sec:");
+            //If this task takes longer than period, then next task will start after the given deal after this repeatation ends
+        }, 4, 2, TimeUnit.SECONDS);
+
+//        try {
+//            Thread.sleep(10000);
+//            futureObj.cancel(true);
+//        } catch (Exception e) {
+//
+//        }
     }
 }
